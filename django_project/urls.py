d48aff81918b0bd75e9f2ccc2826dc933b07c337
Aryan Kumar <kumarayan86@gmail.com>
2019-06-18 01:52:51 +0530

    update on 18-06-2019

--- django_project/urls.py
+++ django_project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from home.views import home_view
 
 urlpatterns = [
+	path('home/',home_view),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
